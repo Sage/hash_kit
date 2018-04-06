81d63b7c18cfc32ae4fbc57adff8e0aa4d53c77b
Chris Barber <chris.barber@sage.com>
2018-04-06 15:09:07 +0100

    Setup coverage, switch to file versioning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,13 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec/'
+end
+
 require 'pry'
 require 'hash_kit'
 require_relative 'hash_kit/test_objects'
 
 RSpec.configure do |config|
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
