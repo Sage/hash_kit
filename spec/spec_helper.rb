5db640071724238a6ad8a16d4209de508bf90c1c
Ian Oxley <ian.oxley@sage.com>
2023-07-28 12:04:42 +0100

    Use simplecov ~> 0.21 and configure JSON formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 require 'simplecov'
+require 'simplecov_json_formatter'
+
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+
 SimpleCov.start do
   add_filter 'spec/'
 end
